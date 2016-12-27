42a5cba12ff6792d4260fa3916e534a5da2eec07
vitorianavi <anavitoria.vitoriano@gmail.com>
2016-12-26 20:45:46 -0400

    Inserção no hash pronta.

--- upload.cpp
+++ upload.cpp
@@ -3,20 +3,43 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define HASH_SIZE 262151
+
 using namespace std;
 
 class Artigo {
 public:
     int id;
-    char titulo[300];
+    char titulo[250];
     int ano;
-    char autores[1024];
+    char autores[50];
     int citacoes;
     char atualizacao[20];
     char snippet[1024];
 };
 
-int parser(const char filename[], vector<Artigo>& artigos) {
+class Bucket {
+public:
+    vector<Artigo> artigos;
+};
+
+Bucket *hash_table;
+
+int hash_function(int id) {
+    int index = id % HASH_SIZE;
+
+    return index;
+}
+
+void insert_hash(Bucket *hash_table, Artigo artigo) {
+    int index = hash_function(artigo.id);
+
+
+    cout << "Inserting " << artigo.id << " in " << index << ".\n";
+    hash_table[index].artigos.push_back(artigo);
+}
+
+int parser(const char filename[]) {
     FILE *arquivo;
     Artigo artigo;
 
@@ -35,7 +58,9 @@ int parser(const char filename[], vector<Artigo>& artigos) {
             fscanf(arquivo, "%[^\n]\n", artigo.snippet);
         }
 
-        artigos.push_back(artigo);
+        insert_hash(hash_table, artigo); //populando hash
+        //artigos.push_back(artigo);
+
         printf("id: %d\n", artigo.id);
         //printf("ID: %d TITULO: %s ANO: %d AUTORES: %s\n", artigo.id, artigo.titulo, artigo.ano, artigo.autores);
         //printf("CITACOES: %d ATUALIZACAO: %s SNIPPET: %s\n", artigo.citacoes, artigo.atualizacao, artigo.snippet);
@@ -50,5 +75,8 @@ int main() {
     vector<Artigo> artigos;
     int status;
 
-    status = parser("artigo.csv", artigos);
+    hash_table = (Bucket*) malloc(sizeof(Bucket)*HASH_SIZE);
+
+    status = parser("artigo.csv");
+    cout << sizeof(Artigo);
 }
